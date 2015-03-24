62d384af8e06983ecbecfff257dd8e97cf773fea
unknown <tomohiro.kono@O-05407-WIN.dena.local>
2015-03-24 10:26:17 +0900

    ビルド更新テスト

--- SpriteEditor/GeneratedFiles/Debug/moc_spriteeditor.cpp
+++ SpriteEditor/GeneratedFiles/Debug/moc_spriteeditor.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_SpriteEditor_t {
-    QByteArrayData data[5];
-    char stringdata[107];
+    QByteArrayData data[6];
+    char stringdata[141];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,12 +32,14 @@ static const qt_meta_stringdata_SpriteEditor_t qt_meta_stringdata_SpriteEditor =
 QT_MOC_LITERAL(0, 0, 12), // "SpriteEditor"
 QT_MOC_LITERAL(1, 13, 27), // "on_actionOpenFile_triggered"
 QT_MOC_LITERAL(2, 41, 0), // ""
-QT_MOC_LITERAL(3, 42, 31), // "on_listWidget_currentRowChanged"
-QT_MOC_LITERAL(4, 74, 32) // "on_pushButton_ClearPoint_clicked"
+QT_MOC_LITERAL(3, 42, 33), // "on_actionSaveOriginalFile_cli..."
+QT_MOC_LITERAL(4, 76, 31), // "on_listWidget_currentRowChanged"
+QT_MOC_LITERAL(5, 108, 32) // "on_pushButton_ClearPoint_clicked"
 
     },
     "SpriteEditor\0on_actionOpenFile_triggered\0"
-    "\0on_listWidget_currentRowChanged\0"
+    "\0on_actionSaveOriginalFile_clicked\0"
+    "on_listWidget_currentRowChanged\0"
     "on_pushButton_ClearPoint_clicked"
 };
 #undef QT_MOC_LITERAL
@@ -48,7 +50,7 @@ static const uint qt_meta_data_SpriteEditor[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -56,11 +58,13 @@ static const uint qt_meta_data_SpriteEditor[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   29,    2, 0x08 /* Private */,
-       3,    1,   30,    2, 0x08 /* Private */,
-       4,    0,   33,    2, 0x08 /* Private */,
+       1,    0,   34,    2, 0x08 /* Private */,
+       3,    0,   35,    2, 0x08 /* Private */,
+       4,    1,   36,    2, 0x08 /* Private */,
+       5,    0,   39,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    2,
     QMetaType::Void,
@@ -74,8 +78,9 @@ void SpriteEditor::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         SpriteEditor *_t = static_cast<SpriteEditor *>(_o);
         switch (_id) {
         case 0: _t->on_actionOpenFile_triggered(); break;
-        case 1: _t->on_listWidget_currentRowChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 2: _t->on_pushButton_ClearPoint_clicked(); break;
+        case 1: _t->on_actionSaveOriginalFile_clicked(); break;
+        case 2: _t->on_listWidget_currentRowChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->on_pushButton_ClearPoint_clicked(); break;
         default: ;
         }
     }
@@ -106,13 +111,13 @@ int SpriteEditor::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
